497ec7518cff92284ab502a5ff2fa445ab8ec702
“dansaround” <kcomtdaniel@gmail.com>
2023-03-09 14:26:21 -0500

    login and routes implemented

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Login from "./components/login/Login";
+import { RouterProvider } from "react-router-dom";
 
+import { router } from "./router";
 import { GameState } from "./context/GameContex";
-
 import { ModalState } from "./context/ModalContext";
 
 ReactDOM.render(
   // <React.StrictMode>
   <ModalState>
     <GameState>
-      <App />
+      <RouterProvider router={router} />
     </GameState>
   </ModalState>,
   // </React.StrictMode>,
